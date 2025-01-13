7ee2e64b68a9771393f754d25eaa3a241e31a2b2
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-01-13 15:02:47 +0200

    DoublyLinkedListNode.java

--- src/main/java/com/cache/DoublyLinkedListNode.java
+++ src/main/java/com/cache/DoublyLinkedListNode.java
@@ -1,1 +1,20 @@
+package com.cache;
+
+/**
+ * A node in a doubly linked list.
+ *
+ * @param <K> the key type
+ * @param <V> the value type
+ */
+class DoublyLinkedListNode<K, V> {
+    K key;
+    V value;
+    DoublyLinkedListNode<K, V> prev;
+    DoublyLinkedListNode<K, V> next;
+
+    DoublyLinkedListNode(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+}
 
