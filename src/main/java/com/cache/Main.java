8a9de470ffc0c41def0541713c8bad8edb601713
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-01-13 15:05:14 +0200

    Main.java

--- src/main/java/com/cache/Main.java
+++ src/main/java/com/cache/Main.java
@@ -1,1 +1,14 @@
+package com.cache;
+
+public class Main {
+    public static void main(String[] args) {
+        Cache<Integer, String> lruCache = new LRUCache<>(3);
+        lruCache.put(1, "One");
+        lruCache.put(2, "Two");
+        lruCache.put(3, "Three");
+        System.out.println(lruCache.get(1)); // Output: One
+        lruCache.put(4, "Four"); // Evicts key 2
+        System.out.println(lruCache.get(2)); // Output: null
+    }
+}
 
